6b139d96bb843a40b07115d0c4109a96ff8b4d59
MOON <moonjs1018@naver.com>
2024-10-14 15:41:02 +0900

    first commit

---
+++ src/main/java/io/rednotice/ReDnoticeApplication.java
@@ -0,0 +1,13 @@
+package io.rednotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReDnoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReDnoticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/rednotice/ReDnoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rednotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReDnoticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
