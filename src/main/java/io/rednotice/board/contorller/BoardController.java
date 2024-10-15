93cd936172b456198a2f901948a13b5eb0565738
ilmechaJu <pawor006@naver.com>
2024-10-15 10:30:11 +0900

    hotfix : wrong upload

--- src/main/java/io/rednotice/board/contorller/BoardController.java
+++ src/main/java/io/rednotice/board/contorller/BoardController.java
@@ -13,31 +13,5 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class BoardController {
-    private final WorkSpaceService workSpaceService;
-
-    // Security 추가 후 admin 권한만 생성되도록 변경 예정
-    @PostMapping("/boards")
-    public ResponseEntity<WorkSpaceResponse> saveWorkSpace(@RequestBody WorkSpaceSaveRequest request) {
-
-        WorkSpaceResponse response = workSpaceService.saveWorkSpace(request);
-
-        return ResponseEntity.ok(response);
-    }
-
-    // 보드 리스트 조회
-    @GetMapping("/boards")
-    public ResponseEntity<List<WorkSpaceResponse>> findAll() {
-
-        return ResponseEntity.ok(workSpaceService.findAll());
-    }
-
-    // 보드 단건 조회
-    @GetMapping("/boards/{boardid}")
-    public ResponseEntity<List<WorkSpaceResponse>> findAll() {
-
-        return ResponseEntity.ok(workSpaceService.findAll());
-    }
-
-    @DeleteMapping("/boards/{boardid}")
 
 }
