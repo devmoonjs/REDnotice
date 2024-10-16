ef80d64669e774e21fcea440c319bb89fb9cb8ba
KimHS <best847283@icloud.com>
2024-10-16 16:51:46 +0900

    feat : GlobalExceptionHandler Authenticationrincipal exception

--- src/main/java/io/rednotice/common/exception/GlobalExceptionHandler.java
+++ src/main/java/io/rednotice/common/exception/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package io.rednotice.common.exception;
 
 import io.rednotice.common.apipayload.ApiResponse;
 import io.rednotice.common.apipayload.BaseCode;
+import io.rednotice.common.apipayload.status.ErrorStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +18,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiResponse<String>> handleAuthenticationException(AuthenticationException e) {
+        return ResponseEntity.status(ErrorStatus._NOT_AUTHENTICATIONPRINCIPAL_USER.getHttpStatus())
+                .body(ApiResponse.fail(ErrorStatus._NOT_AUTHENTICATIONPRINCIPAL_USER));
+    }
+
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ApiResponse<String>> handleCustomException(ApiException e) {
